1ac13e1c918b2bc26be94d2e86a7abe34f835ec6
James Schumann <jschumann@gmail.com>
2019-01-24 09:14:52 -0500

    parse json twice

--- wp-post-store/index.js
+++ wp-post-store/index.js
@@ -1,8 +1,9 @@
 module.exports = function (context, req) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
 
-    var raw_post = req.body;
-    var parsed_post = JSON.parse(raw_post);
+    var raw_body = req.body;
+    var post_string = JSON.parse(raw_body);
+    var parsed_post = JSON.parse(post_string);
 
     parsed_post.post_id = parsed_post.ID;
     parsed_post.ID = `${parsed_post.site_url}-${parsed_post.post_id}`;
