86d057e69c3716a585636cbed42c7fa8bcd90b4d
James Schumann <jschumann@gmail.com>
2019-01-24 10:05:05 -0500

    working code; fixed message being written to queue

--- wp-post-store/index.js
+++ wp-post-store/index.js
@@ -1,21 +1,18 @@
 module.exports = function (context, req) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
 
-    var raw_body = req.body;
-    var post_string = JSON.parse(raw_body);
-    var parsed_post = JSON.parse(post_string);
-
+    var parsed_post = req.body;
     parsed_post.post_id = parsed_post.ID;
-    parsed_post.ID = `${parsed_post.site_url}-${parsed_post.post_id}`;
+    parsed_post.ID = `${parsed_post.site_domain}-${parsed_post.site_slug}-${parsed_post.post_id}`;
     parsed_post.ID = parsed_post.ID.replace(/\./g, '-');
 
     context.bindings.postToStore = JSON.stringify(parsed_post);
 
     context.res = {
         status: 200,
-        body: raw_post
+        body: parsed_post
     };
 
-    context.log(raw_post);
-    context.done(null, raw_post);
+    context.log(parsed_post);
+    context.done(null, parsed_post);
 };
