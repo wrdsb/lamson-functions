b6c798e68c4544d7070cb30db1e9ca69555f543d
James Schumann <jschumann@gmail.com>
2019-01-25 09:14:50 -0500

    updated lamson webhooks plugin. no longer necessary to construct custom post id here.

--- wp-post-store/index.js
+++ wp-post-store/index.js
@@ -2,10 +2,6 @@ module.exports = function (context, req) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
 
     var parsed_post = req.body;
-    parsed_post.post_id = parsed_post.ID;
-    parsed_post.id = `${parsed_post.site_domain}-${parsed_post.site_slug}-${parsed_post.post_id}`;
-    parsed_post.id = parsed_post.id.replace(/\./g, '-');
-    delete parsed_post.ID;
 
     context.bindings.postToStore = JSON.stringify(parsed_post);
 
