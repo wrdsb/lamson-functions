04037c2fcfc276050de0b7a7e3980b9ccfa1e741
James Schumann <jschumann@gmail.com>
2019-02-05 11:29:22 -0500

    missing from address fixed

--- wp-post-notify/index.js
+++ wp-post-notify/index.js
@@ -8,7 +8,7 @@ module.exports = function (context, req) {
 
     var notification = {
         to: 'james_schumann@wrdsb.ca',
-        from: "",
+        from: 'notifications@lamson.wrdsb.io',
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
