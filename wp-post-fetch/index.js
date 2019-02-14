6a83c1b90cbe2e57706ff501a48a79203aba30f4
James Schumann <jschumann@gmail.com>
2019-02-14 15:34:43 -0500

    req object, not data object, coming from request binding

--- wp-post-fetch/index.js
+++ wp-post-fetch/index.js
@@ -1,4 +1,4 @@
-module.exports = function (context, data) {
+module.exports = function (context, req) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
 
     // get request params
