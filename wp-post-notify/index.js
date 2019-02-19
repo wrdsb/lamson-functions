47988c0e041fc8d3ec0afbdc75a9854def099f7e
James Schumann <jschumann@gmail.com>
2019-02-19 13:15:31 -0500

    send to google groups; fileter out non-posts

--- wp-post-notify/index.js
+++ wp-post-notify/index.js
@@ -5,27 +5,32 @@ module.exports = function (context, req) {
     sgMail.setApiKey(process.env['SENDGRID_API_KEY']);
 
     var parsed_post = req.body;
+
+    if (parsed_post.post_type != 'post') {
+        context.done(null, "Not a post.")
+    }
+
     var site_domain = parsed_post.site_domain;
     var site_slug = parsed_post.site_slug;
     var site_name = parsed_post.site_name;
+
     var id = parsed_post.id;
     var post_title = parsed_post.post_title;
     var post_content = parsed_post.post_content;
     var post_guid = parsed_post.post_guid;
 
     var notification = {
-        subject: post_title,
-        html: `${post_content} <p>Originally published on the Staff Intranet here: ${post_guid}</p>`
+        subject: post_title
     };
 
     switch (site_domain) {
         case 'staff.wrdsb.ca':
-            //notification.to = `intranet-${site_slug}@wrdsb.ca`;
-            notification.to = 'james_schumann@wrdsb.ca';
+            notification.to = `intranet-${site_slug}@wrdsb.ca`;
             notification.from = {
                 email: `intranet-${site_slug}@lamson.wrdsb.ca`,
                 name: site_name
             };
+            notifictaion.html = `${post_content} <p>Originally published on the Staff Intranet here: ${post_guid}</p>`
             break;
     
         default:
@@ -34,6 +39,7 @@ module.exports = function (context, req) {
                 email: 'notifications@lamson.wrdsb.io',
                 name: 'Lamson Test Notifications'
             };
+            notifictaion.html = `${post_content} <p>Originally here: ${post_guid}</p>`
             break;
     }
 
