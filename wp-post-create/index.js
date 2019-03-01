59ccefd5fb4e75d4ba891007996b0ddc43315508
James Schumann <jschumann@gmail.com>
2019-03-01 14:03:00 -0500

    wp-post-create now using axios for requests. better control over request and response than wpapi library

--- wp-post-create/index.js
+++ wp-post-create/index.js
@@ -1,5 +1,6 @@
 module.exports = function (context, req) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
+    const axios = require('axios');
 
     // get request params
     var wp_domain = req.body.wp_domain;
@@ -11,28 +12,80 @@ module.exports = function (context, req) {
     // get credentials from environment
     var api_key_name = `wrdsb_${wp_service}_${wp_environment}_key`;
     var api_key = process.env[api_key_name];
-    var api_endpoint = (wp_site) ? `https://${wp_domain}/${wp_site}/wp-json` : `https://${wp_domain}/wp-json`;
 
-    var WPAPI = require( 'wpapi' );
-    var wp = new WPAPI({ endpoint: api_endpoint });
+    // get author id from environment
+    var author_id_name = `wrdsb_${wp_service}_${wp_environment}_author`;
+    post.author = process.env[author_id_name];
+    
+    var baseURL = (wp_site) ? `https://${wp_domain}/${wp_site}/wp-json` : `https://${wp_domain}/wp-json`;
+    var path = '/wp/v2/posts';
 
-    wp.setHeaders( 'Authorization', `Basic ${api_key}` );
+    var options = {
+        baseURL: baseURL,
+        url: path,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${api_key}`
+        },
+        data: post
+    };
 
-    wp.posts().create(post, function( error, data ) {
-        if ( error ) {
+    axios(options)
+    .then(function (response) {
+        //response.config.headers.Authorization = 'redacted';
+        //context.log(response);
+
+        context.log(`${response.status}: ${response.statusText}`);
+
+        // cut off everything up to first {, because WPDEBUG prepends errors as HTML, buggering up the JSON. *sigh*
+        created_post = JSON.parse(response.data.substring(response.data.indexOf('{')));
+
+        context.res = {
+            status: response.status,
+            body: {
+                statusText: response.statusText,
+                data: created_post
+            }
+        };
+    })
+    .catch(function (error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            context.log(error.response.data);
+            context.log(error.response.status);
+            context.log(error.response.headers);
+
+            context.res = {
+                status: error.response.status,
+                body: error.response.data
+            };
+    
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            context.log(error.request);
+
+            context.res = {
+                status: error.request.status,
+                body: error.request
+            };
+
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            context.log('Error', error.message);
+
             context.res = {
                 status: 500,
                 body: error
-            }
-            context.log(error);
-            context.done(error);
-            return;
-        } else {
-            context.res = {
-                status: 200,
-                body: data
-            }
-            context.done(null, data);
+            };
+
         }
+        context.log(error.config);
+    })
+    .then(function () {
+        context.done();
     });
 };
