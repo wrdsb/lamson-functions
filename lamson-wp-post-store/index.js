5835f38d4be74df0db16ee21819f33b600d2446b
James Schumann <jschumann@gmail.com>
2019-05-31 10:19:25 -0400

    evaluate privacy settings and create visibility array

--- lamson-wp-post-store/index.js
+++ lamson-wp-post-store/index.js
@@ -2,6 +2,40 @@ module.exports = function (context, message) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
 
     var parsed_post = message;
+    var site_domain = parsed_post.site_domain;  // "staff.wrdsb.ca"
+    var site_url = parsed_post.site_url;         // "staff.wrdsb.ca/its-team"
+    var visible_to = [];
+
+    switch (parsed_post.site_privacy) {
+        case '-1':
+            visible_to.push(`${site_domain}:members`);
+            visible_to.push(`${site_url}:members`);
+            visible_to.push(`${site_url}:admins`);
+            break;
+        case '-2':
+            visible_to.push(`${site_url}:members`);
+            visible_to.push(`${site_url}:admins`);
+            break;
+        case '-3':
+            visible_to.push(`${site_url}:admins`);
+            break;
+        case '0':
+            visible_to.push(`${site_domain}:members`);
+            visible_to.push(`${site_url}:members`);
+            visible_to.push(`${site_url}:admins`);
+            visible_to.push('public');
+            break;
+        case '1':
+            visible_to.push(`${site_domain}:members`);
+            visible_to.push(`${site_url}:members`);
+            visible_to.push(`${site_url}:admins`);
+            visible_to.push('public');
+            break;
+        default:
+            break;
+    }
+
+    parsed_post.visible_to = visible_to;
 
     context.bindings.postToStore = parsed_post;
 
