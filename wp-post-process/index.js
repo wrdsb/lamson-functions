d92179274b135b7b315d649621fa05b9585c8de7
James Schumann <jschumann@gmail.com>
2019-08-27 08:34:34 -0400

    wrap notifications and syndication in conditional so we can process non-published posts as well

--- wp-post-process/index.js
+++ wp-post-process/index.js
@@ -1,17 +1,21 @@
 module.exports = function (context, message) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
 
+    let post = context.bindings.hookWpPostPublish;
+
     context.log('Queuing storage.');
     context.bindings.lamsonWpPostStore = message;
 
-    if (context.bindings.hookWpPostPublish.lamson_send_notification === 'yes') {
-        context.log('Queuing notifications.');
-        context.bindings.lamsonWpPostNotify = message;
-    }
+    if (post.post_status === 'publish') {
+        if (post.lamson_send_notification === 'yes') {
+            context.log('Queuing notifications.');
+            context.bindings.lamsonWpPostNotify = message;
+        }
 
-    if (context.bindings.hookWpPostPublish.lamson_do_syndication === 'yes') {
-        context.log('Queuing syndication.');
-        context.bindings.lamsonWpPostSyndicate = message;
+        if (post.lamson_do_syndication === 'yes') {
+            context.log('Queuing syndication.');
+            context.bindings.lamsonWpPostSyndicate = message;
+        }
     }
 
     context.done(null, message);
